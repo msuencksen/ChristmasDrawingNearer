38c789b9bedc395fc361ae9904da49bb3c0ce89a
Matthias Süncksen <m.suencksen@web.de>
2020-01-16 00:27:45 +0100

    Some sound experiment

--- Assets/Audio/README.txt
+++ Assets/Audio/README.txt
@@ -2,3 +2,5 @@
 http://soundbible.com/634-Wind-Blowing.html  Attribution 3.0
 
 http://soundbible.com/2103-1-Person-Cheering.html
+
+http://soundbible.com/969-Pat.html
--- Assets/Parcel.cs
+++ Assets/Parcel.cs
@@ -4,24 +4,60 @@ using UnityEngine;
 
 public class Parcel : MonoBehaviour
 {
-    public AudioSource audioSource;
     public AudioClip cheers;
 
+    public AudioSource[] audioSource;
+
     // Start is called before the first frame update
-    void Awake()
+    void Start()
     {
-     
+        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.P))
+            audioSource[1].PlayOneShot(audioSource[1].clip);
+
+        playPause = audioSource[1].isPlaying;
+
+        Debug.Log(playPause);
     }
+
+    private bool playPause;
+
+    void OnCollisionEnter(Collision other)
+    {
+        //Debug.Log(other.gameObject.name + " " + other.gameObject.tag);
+
+        if (other.gameObject.tag == "Sleigh")
+        {
+            if (transform.parent == null)
+            {
+                transform.parent = other.gameObject.transform;
+            }
+        }
+
+        //if (other.gameObject.tag == "Untagged")
+        //{
+        //    if (!playPause)
+        //    {
+        //        audioSource[1].Play();
+        //        Debug.Log("Play " + Time.time);
+        //        playPause = true;
+        //     }
+
+        
+        //}
+    }
+
 
     private void OnTriggerEnter(Collider other)
     {
 
+       
         if (other.tag == "Chimney")
         {
 
@@ -29,10 +65,13 @@ public class Parcel : MonoBehaviour
 
             transform.position = other.transform.parent.position; // should be base of house
 
-            audioSource.Play();
+            audioSource[0].Play();
 
-            //AudioSource.PlayClipAtPoint(cheers, other.transform.parent.position, 2); // no spatializing ?
-        
+            //AudioSource.PlayClipAtPoint(cheers, other.transform.parent.position, 2); // no spatializing ?      
         }
+
+       
+      
+
     }
 }
